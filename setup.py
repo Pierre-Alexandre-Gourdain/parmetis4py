dbeef5125a489d565ec4148b1a900fa33191bead
Pierre Gourdain <42790471+Pierre-Alexandre-Gourdain@users.noreply.github.com>
2025-10-02 08:42:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,135 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import os
+
+__version__ = '0.0.1'
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+try:
+    petsc_dir = os.environ['PETSC_DIR']
+except KeyError:
+    raise RuntimeError("Please set PETSC_DIR environment variable.")
+try:
+    petsc_arch = os.environ['PETSC_ARCH']
+except KeyError:
+    raise RuntimeError("Please set PETSC_ARCH environment variable.")
+
+
+
+petsc_build_dir = os.path.join(petsc_dir, petsc_arch)
+parmetis_inc_dir = os.path.join(petsc_build_dir, 'include')
+parmetis_lib_dir = os.path.join(petsc_build_dir, 'lib')
+
+metis_inc_dir = os.path.join(petsc_build_dir, 'include')
+
+try:
+    mpi_dir = os.environ['MPI_DIR']
+except KeyError:
+    raise RuntimeError("Please set MPI_DIR environment variable (will look for $MPI_DIR/include and $MPI_DIR/lib).")
+mpi_inc_dir = os.path.join(mpi_dir, 'include')
+mpi_lib_dir = os.path.join(mpi_dir, 'lib')
+
+
+ext_modules = [
+    Extension(
+        'parmetis4py',
+        ['src/main.cpp'],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True),
+            parmetis_inc_dir,
+            mpi_inc_dir,
+            metis_inc_dir
+        ],
+        libraries=['parmetis'],#,'mpich','opa','mpl','rt','pthread'], #TODO
+        library_dirs=[parmetis_lib_dir, mpi_lib_dir],
+        runtime_library_dirs=[parmetis_lib_dir, mpi_lib_dir],
+        language='c++'
+    ),
+]
+
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14] compiler flag.
+
+    The c++14 is prefered over c++11 (when it is available).
+    """
+    if has_flag(compiler, '-std=c++14'):
+        return '-std=c++14'
+    elif has_flag(compiler, '-std=c++11'):
+        return '-std=c++11'
+    else:
+        raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                           'is needed!')
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+            elif ct == 'msvc':
+                opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+setup(
+    name='parmetis4py',
+    version=__version__,
+    author='Florian Wechsung',
+    author_email='wechsung@maths.ox.ac.uk',
+    description='Simple bindings for parmetis',
+    long_description='',
+    ext_modules=ext_modules,
+    install_requires=['pybind11>=2.2'],
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
+)
